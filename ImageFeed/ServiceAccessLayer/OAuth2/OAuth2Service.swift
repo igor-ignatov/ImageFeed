f73db2c60970a50ec26db30efa90e5c33cadff7d
Igor Ignatov <kantorawb@gmail.com>
2023-04-26 10:16:59 +0400

    fix: add missing files

---
+++ ImageFeed/ServiceAccessLayer/OAuth2/OAuth2Service.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+
+final class OAuth2Service {
+    private enum NetworkError: Error {
+        case codeError
+    }
+    
+    func fetchOAuthToken(_ code: String, completion: @escaping (Result<String, Error>) -> Void) {
+        var urlComponents = URLComponents(string: "https://unsplash.com/oauth/token")
+        
+        urlComponents?.queryItems = [
+            URLQueryItem(name: "client_id", value: AccessKey),
+            URLQueryItem(name: "client_secret", value: SecretKey),
+            URLQueryItem(name: "redirect_uri", value: RedirectURI),
+            URLQueryItem(name: "code", value: code),
+            URLQueryItem(name: "grant_type", value: "authorization_code")
+        ]
+        
+        guard let url = urlComponents?.url else { return }
+         
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                completion(.failure(error ?? URLError(.badServerResponse)))
+                
+                return
+            }
+            
+            do {
+                let responseBody = try JSONDecoder().decode(OAuth2TokenResponseBody.self, from: data)
+                completion(.success(responseBody.accessToken))
+            } catch let error {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+}
